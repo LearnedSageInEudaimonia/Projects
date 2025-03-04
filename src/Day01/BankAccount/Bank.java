9cc65fd37d3515d021d1d2a689d343f9a4817221
aat <learnedsageateudaimonia@gmail.com>
2025-03-04 08:18:34 +0530

    Day 1 Project 1 completed

---
+++ src/Day01/BankAccount/Bank.java
@@ -0,0 +1,53 @@
+package Day01.BankAccount;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Enter the Account Number : ");
+        String accountNumber = read.nextLine();
+
+        System.out.print("Enter the Account Holder Name : ");
+        String accountName = read.nextLine();
+
+        System.out.print("Enter the initial balance : ");
+        Double balance = read.nextDouble();
+
+        BankAccount bankAccount = new BankAccount(accountNumber, accountName, balance);
+
+        while(true){
+            System.out.println("\n<----Banking System---->\n");
+            System.out.println("\t1. Deposit");
+            System.out.println("\t2. Withdraw");
+            System.out.println("\t3. Balance Info");
+            System.out.println("\t4. Exit\n");
+            System.out.print("Choose an option : " );
+
+            int choice = read.nextInt();
+
+            switch(choice){
+                case 1 :
+                    System.out.print("Enter the deposit Amount : ");
+                    int depositAmount = read.nextInt();
+                    bankAccount.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.print("Enter the Amount to Withdraw : ");
+                    int withdrawAmount = read.nextInt();
+                    bankAccount.withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    bankAccount.displayAccount();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using our services.\nHave a Nice day");
+                    read.close();
+                    return;
+                default:
+                    System.out.println("Invalid Choice Please select again!");
+            }
+        }
+    }
+}
---
+++ src/Day01/BankAccount/BankAccount.java
@@ -0,0 +1,45 @@
+package Day01.BankAccount;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountName;
+
+    private Double balance;
+
+    public BankAccount(String accountNumber, String accountName, Double balance) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        if(amount > 0 ){
+            balance += amount;
+            System.out.println(STR."$\{amount} deposited\n");
+        }
+        else{
+            System.out.println("Invalid amount\n");
+        }
+    }
+
+    public void withdraw(double amount){
+        if(amount > 0 && amount <= balance){
+            balance -= amount;
+            System.out.println(STR."$\{amount} withdrawn\n");
+        }
+        else{
+            System.out.println("Insufficient funds or Invalid amount\n");
+        }
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void displayAccount(){
+        System.out.println(STR."Account Number : \{accountNumber}");
+        System.out.println(STR."Account Holder Name : \{accountName}");
+        System.out.println(STR."Balance : \{getBalance()}\n");
+    }
+
+}
