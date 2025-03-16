a8309069c064d41e56b16d076c4ad84bd74bec05
aat <learnedsageateudaimonia@gmail.com>
2025-03-16 19:55:49 +0530

    added DatabaseConnection

--- src/Level_4/StudentManagementSystemUsingJBDC/DatabaseConnection.java
+++ src/Level_4/StudentManagementSystemUsingJBDC/DatabaseConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/students";
     private static final String USER = "root";
-    private static final String PASSWORD = "Eudaimonia";
+    private static final String PASSWORD = "********";
 
     public static Connection getConnection(){
         try{
